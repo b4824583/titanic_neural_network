8b57d062db95aba2e64cee234dbd2e421e7ee1ae
b4824583 <b4824583@gmail.com>
2022-04-09 15:57:25 +0800

    finish the code
    update the code and finish the neural network and output the submit

--- titanic_multi-layer_perceptrons.py
+++ titanic_multi-layer_perceptrons.py
@@ -46,33 +46,33 @@ df_train['Fare'].fillna( df_train.Fare.median(), inplace=True )
 #[3] fare
 #[4] family
 #[5] Embarked
-input_data=np.zeros((891,7))
-input_data[:,0]=1.0
+input_data=np.ones((891,7))
+# input_data[:,0]=1.0
 # print(input_data[0])
 for i in range(891):
     if(df_train["Sex"][i]=="male"):
+        input_data[i][0]=1
+    else:
+        input_data[i][0]=0
+    if(df_train["Age"][i]>=16):
         input_data[i][1]=1
     else:
         input_data[i][1]=0
-    if(df_train["Age"][i]>=16):
-        input_data[i][2]=1
-    else:
-        input_data[i][2]=0
     
-    input_data[i][3]=df_train["Pclass"][i]
-    input_data[i][4]=df_train["Fare"][i]
-    input_data[i][5]=df_train["SibSp"][i]+df_train["Parch"][i]
+    input_data[i][2]=df_train["Pclass"][i]
+    input_data[i][3]=df_train["Fare"][i]
+    input_data[i][4]=df_train["SibSp"][i]+df_train["Parch"][i]
     # if(df_train[""])
     if(df_train["Embarked"][i]=="S"):
-        input_data[i][6]=1
+        input_data[i][5]=1
     elif(df_train["Embarked"][i]=="C"):
-        input_data[i][6]=2
+        input_data[i][5]=2
     elif(df_train["Embarked"][i]=="Q"):
-        input_data[i][6]=3
+        input_data[i][5]=3
 
     # print(input_data[i])
-weight=np.random.rand(7)
-output=np.zeros((891,1),dtype=float)
+# weight=np.random.rand(7)
+# output=np.zeros((891,1),dtype=float)
 
 ###### how to implement aggregation perceptrons?
 
@@ -106,16 +106,16 @@ def forward(s3,w3,x2,s2,w2,x1,s1,w1,x0):
 
 def backward():
     return 0
-def backward_1L(x0,w1,s1,w2,delta2):
+def backward_1L(x0,w1,s1,w2,delta_2):
     delta_1=np.zeros(s1.shape[0])
 
     """ compute delta """
 
     for j in range(s1.shape[0]):
         sum=0
-        for k in range(s2.shape[0]):
-            sum=sum+delta_2[k+1]*w2[k+1][j+1]*tanh_differential(s1[j])
-
+        for k in range(w2.shape[0]):
+            sum=sum+delta_2[k]*w2[k][j]*tanh_differential(s1[j])
+        delta_1[j]=sum
 
     # for j in range(w1.shape[0]):
     #     sum=0
@@ -124,13 +124,22 @@ def backward_1L(x0,w1,s1,w2,delta2):
     #     delta_1[j]=sum
     
     """ update weight """
-    for j in range(s2.shape[0]):
-        for i in range(x1.shape[0]):
+    # for k in range(delta_1.shape[0]):
+    #     for j in range(delta_1.shape[1]):
+    #         w1[]
+    for j in range(s1.shape[0]):
+        for i in range(x0.shape[0]):
             w1[j][i]=w1[j][i]-lr*delta_1[j]*x0[i]
+    # for j in range(s2.shape[0]):
+    #     for i in range(x1.shape[0]):
+    #         w1[j][i]=w1[j][i]-lr*delta_1[j]*x0[i]
 
 
-    return delta_1,w2
+    return delta_1,w1
 #--------------------------------------------- may be this is faliure
+# need check
+# check done
+
 def backward_2L(x1,w2,s2,w3,delta_3):
     delta_2=np.zeros(s2.shape[0])
     
@@ -192,39 +201,95 @@ def main():
     #s3=x2.t.dot(w2)
     #s3=ans
     s3=0.0
-    
-    for idx in range(10):
+    for epoch in range(5000):
         # idx=1
-        if(df_train["Survived"][idx]==1):
-            y=1
-        else:
-            y=-1
-        x0=input_data[idx]
-        print("idx:\t"+str(idx)+"-------------------------")
-        for epoch in range(100):
+        accurate=0.0
+        correct=0
+        loss=0.0
+
+        for idx in range(input_data.shape[0]):
+            if(df_train["Survived"][idx]==1):
+                y=1
+            else:
+                y=-1
+            x0=input_data[idx]
             s3,w3,x2,s2,w2,x1,s1,w1=forward(s3,w3,x2,s2,w2,x1,s1,w1,x0)
-            print("loss:\t"+'%.6f' % (y-s3)**2+"\ty:\t"+'%.6f' % y+"\t s3:\t"+'%.6f' % s3)
+            # print("idx:\t"+str(idx)+"\t\tloss:\t"+'%.6f' % (y-s3)**2)
+            loss+=(y-s3)**2
             # print("y:\t"+'%.6f' % y)
             # print("s3:\t"+'%.6f' % s3)
             delta_3,w3=backward_3L(x2,w3,s3,y)
             delta_2,w2=backward_2L(x1,w2,s2,w3,delta_3)
-    # delta_1=backward_1L(delta_2,x0,w1,s1)
-    # print("partial differential:\ts3")
-    # print(par_diff_w2)
+            delta_1,w1=backward_1L(x0,w1,s1,w2,delta_2)
+            if(y==1):
+                if(s3>=0):
+                    correct+=1
+                else:
+                    pass
+            if(y==-1):
+                if(s3>=0):
+                    pass
+                else:
+                    correct+=1
+        loss=loss/input_data.shape[0]
+        accurate=correct/input_data.shape[0]
+        print("epoch:\t"+str(epoch)+"\t"+str(correct)+"/"+str(input_data.shape[0])+"\tacc:\t\t"+str(accurate)+"\tloss:\t\t"+str(loss))
+        """ test process """
+        if(correct>=720):
+            test=np.ones((df_test.shape[0],7))
+            answer=np.zeros((df_test.shape[0],2),dtype=int)
+            for i in range(df_test.shape[0]):
+                if(df_test["Sex"][i]=="male"):
+                    test[i][0]=1
+                else:
+                    test[i][0]=0
+                if(df_test["Age"][i]>=16):
+                    test[i][1]=1
+                else:
+                    test[i][1]=0
+                
+                test[i][2]=df_test["Pclass"][i]
+                test[i][3]=df_test["Fare"][i]
+                test[i][4]=df_test["SibSp"][i]+df_test["Parch"][i]
+                # if(df_train[""])
+                if(df_test["Embarked"][i]=="S"):
+                    test[i][5]=1
+                elif(df_test["Embarked"][i]=="C"):
+                    test[i][5]=2
+                elif(df_test["Embarked"][i]=="Q"):
+                    test[i][5]=3
+
+
+                x0=test[i]
+                s3,w3,x2,s2,w2,x1,s1,w1=forward(s3,w3,x2,s2,w2,x1,s1,w1,x0)
+                if(s3>=0):
+                    survived=1
+                else:
+                    survived=0
+                answer[i][1]=survived
+            answer[:,0]=df_test["PassengerId"]
+            answer_csv={"PassengerId":answer[:,0],"Survived":answer[:,1]}
+            answer_csv_df = pd.DataFrame(answer_csv)
+            answer_csv_df.to_csv("kaggel_answer.csv",index=False)    
+            break
+        # print()
+        # print()
+        # print()
+        # s3,w3,x2,s2,w2,x1,s1,w1=forward(s3,w3,x2,s2,w2,x1,s1,w1,x0)
     
-    if(y==1):
-        print("ans:\tsurvived")
-    else:
-        print("ans:\tdead")
-    if(s3>0):
-        print("pre:\tsurvive")
-    else:
-        print("pre:\tdead")
+    # if(y==1):
+    #     print("ans:\tsurvived")
+    # else:
+    #     print("ans:\tdead")
+    # if(s3>0):
+    #     print("pre:\tsurvive")
+    # else:
+    #     print("pre:\tdead")
     # print(output)
     # print("loss:\t"+str((y-s3)**2))
     #D
-    """ mse """
-    print("loss:\t"+'%.6f' % (y-s3)**2)
+    # """ mse """
+    # print("loss:\t"+'%.6f' % (y-s3)**2)
     # exit()
 if __name__ == '__main__':
     main()
